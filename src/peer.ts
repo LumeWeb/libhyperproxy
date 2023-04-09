5c666d38c0d352b321a9c93bec4e336929bd72fd
Derrick Hammer <derrick@derrickhammer.com>
2023-04-09 12:15:23 -0400

    *Need to async fetch rawStream

--- src/peer.ts
+++ src/peer.ts
@@ -101,9 +101,10 @@ export default class Peer {
   async init() {
     const self = this;
     let pipe;
+    const raw = await maybeGetAsyncProperty(self._peer.rawStream);
     this._socket = new Socket({
-      remoteAddress: self._peer.rawStream.remoteHost,
-      remotePort: self._peer.rawStream.remotePort,
+      remoteAddress: raw.remoteHost,
+      remotePort: raw.remotePort,
       remotePublicKey: self._peer.remotePublicKey,
       async write(data: any, cb: Function) {
         if (pipe) {
@@ -156,4 +157,24 @@ export default class Peer {
     await this._onchannel?.(this._channel);
     await this._channel.open();
   }
+}
+
+async function maybeGetAsyncProperty(object: any) {
+  if (typeof object === "function") {
+    object = object();
+  }
+
+  if (isPromise(object)) {
+    object = await object;
+  }
+
+  return object;
+}
+
+function isPromise(obj: Promise<any>) {
+  return (
+    !!obj &&
+    (typeof obj === "object" || typeof obj === "function") &&
+    typeof obj.then === "function"
+  );
 }
