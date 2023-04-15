b2b041c3a1ab67402dea953477a91ab8b6675c4d
Derrick Hammer <derrick@derrickhammer.com>
2023-04-15 19:34:45 -0400

    *override callbacks

--- src/proxies/multiSocket.ts
+++ src/proxies/multiSocket.ts
@@ -80,6 +80,9 @@ export default class MultiSocketProxy extends Proxy {
       createDefaultMessage: false,
       ...options,
     });
+    this._socketOptions.onchannel = this.handleNewPeerChannel.bind(this);
+    this._socketOptions.onclose = this.handleClosePeer.bind(this);
+    this._socketOptions.onopen = this.handlePeer.bind(this);
     if (options.socketClass) {
       this.socketClass = options.socketClass;
     } else {
