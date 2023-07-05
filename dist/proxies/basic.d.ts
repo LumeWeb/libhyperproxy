8797249ae51e726ce0b834149e63008ffa666081
Derrick Hammer <derrick@derrickhammer.com>
2023-07-05 06:17:23 -0400

    refactor: switch to new devops

--- dist/proxies/basic.d.ts
+++
@@ -1,5 +0,0 @@
-import Proxy from "../proxy.js";
-import { DataSocketOptions, PeerOptions } from "../peer.js";
-export default class BasicProxy extends Proxy {
-    protected handlePeer({ peer, muxer, ...options }: DataSocketOptions & PeerOptions): void;
-}
--- dist/util.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function idFactory(start: number, step?: number, limit?: number): () => number;
-export declare function roundRobinFactory<T>(list: Map<string, any>): () => T;
-export declare function maybeGetAsyncProperty(object: any): Promise<any>;
-export declare function isPromise(obj: Promise<any>): boolean;
